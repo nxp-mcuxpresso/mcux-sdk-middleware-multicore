77620ec241bd0c0aa747a730865489fb0f6a3284
Michal Princ (nxa17570) <michal.princ@nxp.com>
2021-07-16 16:15:32 +0200

    MulticoreSDK 2.10.0 release

--- middleware_multicore_erpc_eRPC_server.cmake
+++ middleware_multicore_erpc_eRPC_server.cmake
@@ -8,8 +8,7 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/erpc/erpc_c/setup/erpc_server_setup.cpp
 )
 
-
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/erpc/erpc_c/infra
     ${CMAKE_CURRENT_LIST_DIR}/erpc/erpc_c/setup
 )
---
+++ middleware_multicore_rpmsg_lite_imxrt1170_freertos.cmake
@@ -0,0 +1,15 @@
+#Description: Remote Procedure Messaging Lite porting layer for evkmimxrt1170 board; user_visible: False
+include_guard(GLOBAL)
+message("middleware_multicore_rpmsg_lite_imxrt1170_freertos component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/rpmsg_lite/lib/rpmsg_lite/porting/platform/imxrt1170/rpmsg_platform.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/rpmsg_lite/lib/include/platform/imxrt1170
+)
+
+
+include(middleware_multicore_rpmsg_lite)
+include(middleware_freertos-kernel_heap_4)
