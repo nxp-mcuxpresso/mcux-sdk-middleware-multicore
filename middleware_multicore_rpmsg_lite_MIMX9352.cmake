317403d078cbe6bfb343afd33efa7da27a1efc4c
Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
2023-05-26 20:23:00 +0200

    [HRPN-829] add cmake to support rpmsg-lite on MIMX9352
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>

---
+++ middleware_multicore_rpmsg_lite_MIMX9352.cmake
@@ -0,0 +1,14 @@
+#Description: Remote Procedure Messaging Lite library; user_visible: False
+include_guard(GLOBAL)
+message("middleware_multicore_rpmsg_lite component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/rpmsg_lite/lib/common/llist.c
+    ${CMAKE_CURRENT_LIST_DIR}/rpmsg_lite/lib/rpmsg_lite/rpmsg_lite.c
+    ${CMAKE_CURRENT_LIST_DIR}/rpmsg_lite/lib/rpmsg_lite/rpmsg_ns.c
+    ${CMAKE_CURRENT_LIST_DIR}/rpmsg_lite/lib/virtio/virtqueue.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/rpmsg_lite/lib/include
+)
